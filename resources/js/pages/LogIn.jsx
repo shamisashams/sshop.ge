4229e62550ddc55d29ea85333600754bdd4672b2
Alex Zhukov <alex3dfree@gmail.com>
2022-10-24 16:45:41 +0400

    update fix

--- resources/js/pages/LogIn.jsx
+++ resources/js/pages/LogIn.jsx
@@ -32,6 +32,7 @@ const LogIn = ({seo}) => {
         // If the user presses the "Enter" key on the keyboard
         if (event.key === "Enter") {
             // Cancel the default action, if needed
+
             event.preventDefault();
             // Trigger the button element with a click
             Inertia.post(route("client.login"), values);
