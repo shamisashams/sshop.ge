a90aac28b6f248b83b6ea9d88371f950d694b029
Alex Zhukov <alex3dfree@gmail.com>
2022-10-24 11:01:57 +0400

    update fix

--- app/Http/Controllers/Client/ProductController.php
+++ app/Http/Controllers/Client/ProductController.php
@@ -273,7 +273,7 @@ class ProductController extends Controller
             ->leftJoin('product_categories', 'product_categories.product_id', '=', 'products.id')
             ->inRandomOrder()
             ->groupBy('products.id')
-            ->with('latestImage','variants')->get();
+            ->with('latestImage','variants','translation','attribute_values.attribute.translation','attribute_values.attribute.options.translation')->get();
 
         foreach ($similar_products as $_product){
             $product_attributes = $_product->attribute_values;
