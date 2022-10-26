c139a0f5350e2d83dba219dec21e6ddaf08f10af
Alex Zhukov <alex3dfree@gmail.com>
2022-10-26 16:25:54 +0400

    update fix

--- resources/views/client/base.blade.php
+++ resources/views/client/base.blade.php
@@ -108,32 +108,37 @@
     <!-- End Meta Pixel Code -->
 </head>
 <body>
-<!-- Messenger Chat Plugin Code -->
-{{--<div id="fb-root"></div>--}}
 
-<!-- Your Chat Plugin code -->
-{{--<div id="fb-customer-chat" class="fb-customerchat"></div>--}}
+<!-- Messenger Chat plugin Code -->
+<div id="fb-root"></div>
+
+<!-- Your Chat plugin code -->
+<div id="fb-customer-chat" class="fb-customerchat">
+</div>
+
 
 <script>
-    /*var chatbox = document.getElementById('fb-customer-chat');
-    chatbox.setAttribute("page_id", "100160159275529");
+    var chatbox = document.getElementById('fb-customer-chat');
+    chatbox.setAttribute("page_id", "105464005602533");
     chatbox.setAttribute("attribution", "biz_inbox");
+</script>
 
-    window.fbAsyncInit = function () {
+<!-- Your SDK code -->
+<script>
+    window.fbAsyncInit = function() {
         FB.init({
-            xfbml: true,
-            version: 'v12.0'
+            xfbml            : true,
+            version          : 'v15.0'
         });
     };
 
-    (function (d, s, id) {
+    (function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
-        js = d.createElement(s);
-        js.id = id;
+        js = d.createElement(s); js.id = id;
         js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
         fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));*/
+    }(document, 'script', 'facebook-jssdk'));
 </script>
 @inertia
 
