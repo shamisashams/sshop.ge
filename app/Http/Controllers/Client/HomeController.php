118fdbdbd7b7136a15f6102df37bd118af156ac5
Alex Zhukov <alex3dfree@gmail.com>
2022-11-24 10:30:13 +0400

    update

--- app/Http/Controllers/Client/HomeController.php
+++ app/Http/Controllers/Client/HomeController.php
@@ -38,54 +38,17 @@ class HomeController extends Controller
         $sliders = Slider::query()->where("status", 1)->with(['file', 'translations','desktop','mobile'])->get();
 //        dd($page->file);
         //dd($sliders);
-        $_products = app(ProductRepository::class)->getHomePageProducts();
-
-        $products = [];
-        $products['new'] = [];
-        $products['bunker'] = [];
-        $products['day_product'] = [];
-        $products['day_price'] = [];
-        $products['special_price_tag'] = [];
-        $products['popular'] = [];
-        $products['rand_products'] = [];
-        foreach ($_products as $product){
-            $product_attributes = $product->attribute_values;
-
-            $_result = [];
-
-            foreach ($product_attributes as $item){
-                //$options = $item->attribute->options;
-                $value = '';
-                /*foreach ($options as $option){
-                    if($item->attribute->type == 'select'){
-                        if($item->integer_value == $option->id) {
-                            $_result[$item->attribute->code] = $option->label;
-                        }
-
-                    }
-                }*/
-                if($item->attribute->type == 'select'){
-
-                    $_result[$item->attribute->code] = $item->option->label;
+        //$_products = app(ProductRepository::class)->getHomePageProducts();
 
 
-                }
-            }
 
 
-            $product['attributes'] = $_result;
 
-            if($product->new) $products['new'][] = $product;
-            if($product->bunker) $products['bunker'][] = $product;
-            if($product->day_product) $products['day_product'][] = $product;
-            if($product->day_price) $products['day_price'][] = $product;
-            if($product->special_price_tag) $products['special_price_tag'][] = $product;
-            if($product->popular) $products['popular'][] = $product;
-        }
+
 
         $rand_products =  Product::with(['latestImage','translation','attribute_values.attribute.translation','attribute_values.option.translation'])->whereHas('categories',function ($query){
             $query->where('status',1);
-        })->inRandomOrder()->limit(25)->get();
+        })->inRandomOrder()->limit(15)->get();
 
         foreach ($rand_products as $product){
             $product_attributes = $product->attribute_values;
@@ -116,6 +79,108 @@ class HomeController extends Controller
 
         $products['rand_products'] = $rand_products;
         //dd($products);
+
+
+        $special_products =  Product::with(['latestImage','translation','attribute_values.attribute.translation','attribute_values.option.translation'])->whereHas('categories',function ($query){
+            $query->where('status',1);
+        })->where('special_price_tag',1)->inRandomOrder()->limit(15)->get();
+
+        foreach ($special_products as $product){
+            $product_attributes = $product->attribute_values;
+
+            $_result = [];
+
+            foreach ($product_attributes as $item){
+                //$options = $item->attribute->options;
+                $value = '';
+                /*foreach ($options as $option){
+                    if($item->attribute->type == 'select'){
+                        if($item->integer_value == $option->id) {
+                            $_result[$item->attribute->code] = $option->label;
+                        }
+
+                    }
+                }*/
+
+                if($item->attribute->type == 'select'){
+
+                    $_result[$item->attribute->code] = $item->option->label;
+
+
+                }
+            }
+            $product['attributes'] = $_result;
+        }
+
+        $products['special_price_tag'] = $special_products;
+
+
+        $popular_products =  Product::with(['latestImage','translation','attribute_values.attribute.translation','attribute_values.option.translation'])->whereHas('categories',function ($query){
+            $query->where('status',1);
+        })->where('popular',1)->inRandomOrder()->limit(15)->get();
+
+        foreach ($popular_products as $product){
+            $product_attributes = $product->attribute_values;
+
+            $_result = [];
+
+            foreach ($product_attributes as $item){
+                //$options = $item->attribute->options;
+                $value = '';
+                /*foreach ($options as $option){
+                    if($item->attribute->type == 'select'){
+                        if($item->integer_value == $option->id) {
+                            $_result[$item->attribute->code] = $option->label;
+                        }
+
+                    }
+                }*/
+
+                if($item->attribute->type == 'select'){
+
+                    $_result[$item->attribute->code] = $item->option->label;
+
+
+                }
+            }
+            $product['attributes'] = $_result;
+        }
+
+        $products['popular'] = $popular_products;
+
+
+        $new_products =  Product::with(['latestImage','translation','attribute_values.attribute.translation','attribute_values.option.translation'])->whereHas('categories',function ($query){
+            $query->where('status',1);
+        })->where('new',1)->inRandomOrder()->limit(15)->get();
+
+        foreach ($new_products as $product){
+            $product_attributes = $product->attribute_values;
+
+            $_result = [];
+
+            foreach ($product_attributes as $item){
+                //$options = $item->attribute->options;
+                $value = '';
+                /*foreach ($options as $option){
+                    if($item->attribute->type == 'select'){
+                        if($item->integer_value == $option->id) {
+                            $_result[$item->attribute->code] = $option->label;
+                        }
+
+                    }
+                }*/
+
+                if($item->attribute->type == 'select'){
+
+                    $_result[$item->attribute->code] = $item->option->label;
+
+
+                }
+            }
+            $product['attributes'] = $_result;
+        }
+
+        $products['new'] = $new_products;
 
         return Inertia::render('Home', ["sliders" => $sliders,
             "partners" => Partner::query()->orderBy('company_name')->get(),
