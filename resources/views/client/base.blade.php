a37967abb187613661f54572b39cec132a9cbaff
Alex Zhukov <alex3dfree@gmail.com>
2022-12-09 12:24:13 +0400

    update fix 5

--- resources/views/client/base.blade.php
+++ resources/views/client/base.blade.php
@@ -118,11 +118,11 @@
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '1295716787893311');
+        fbq('init', '709074254170946');
         fbq('track', 'PageView');
     </script>
     <noscript><img height="1" width="1" style="display:none"
-                   src="https://www.facebook.com/tr?id=1295716787893311&ev=PageView&noscript=1"
+                   src="https://www.facebook.com/tr?id=709074254170946&ev=PageView&noscript=1"
         /></noscript>
     <!-- End Meta Pixel Code -->
 
