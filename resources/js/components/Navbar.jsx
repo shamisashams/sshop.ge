4860b2bb8648478ca822496779b2c320f9450014
insitegedev <70265828+insitegedev@users.noreply.github.com>
2023-02-02 14:55:30 +0400

    update big

--- resources/js/components/Navbar.jsx
+++ resources/js/components/Navbar.jsx
@@ -214,7 +214,7 @@ const Navbar = () => {
                                 />
                             </svg>
                             <p className="text-xs whitespace-nowrap text-custom-blue absolute left-1/2 -translate-x-1/2 top-full group-hover:opacity-100 opacity-0 transition-all duration-300">
-                                {user? __("client.auth_welcome", localizations) + ' ' + user.name + ' ' + user.surname :__("client.signin_up", localizations)}
+                                {user? __("client.auth_welcome", localizations) + ' ' + user.name :__("client.signin_up", localizations)}
                             </p>
                         </Link>
                         <Link
