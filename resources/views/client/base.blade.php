6827fb58e6704e7bfb788c7ddd0f4b651cf4e3d4
insitegedev <70265828+insitegedev@users.noreply.github.com>
2023-02-10 17:37:28 +0400

    update big

--- resources/views/client/base.blade.php
+++ resources/views/client/base.blade.php
@@ -126,6 +126,18 @@
         /></noscript>
     <!-- End Meta Pixel Code -->
 
+    <!-- Hotjar Tracking Code for https://sshop.ge/ge -->
+    <script>
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3361415,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
+
 </head>
 <body>
 
